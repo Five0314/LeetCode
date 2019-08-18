b4c53b151118a6138c86c146293d6a6be0420807
奇异的炸酱面 <875169560@qq.com>
2019-08-18 18:14:59 +0800

    19. 删除链表的倒数第N个节点

--- LeetCode/ViewController.swift
+++ LeetCode/ViewController.swift
@@ -25,24 +25,36 @@ class ViewController: UIViewController {
 //        let n4: TreeNode = TreeNode.init(0)
 //        let n5: TreeNode = TreeNode.init(-6)
         
-        let n1: TreeNode = TreeNode.init(-2)
-        let n2: TreeNode = TreeNode.init(-1)
-        let n3: TreeNode = TreeNode.init(-9)
-        let n4: TreeNode = TreeNode.init(-8)
-        let n5: TreeNode = TreeNode.init(3)
-        let n6: TreeNode = TreeNode.init(-2)
+//        let n1: ListNode = ListNode.init(1)
+//        let n2: TreeNode = TreeNode.init(-1)
+//        let n3: TreeNode = TreeNode.init(-9)
+//        let n4: TreeNode = TreeNode.init(-8)
+//        let n5: TreeNode = TreeNode.init(3)
+//        let n6: TreeNode = TreeNode.init(-2)
         
-        n1.right = n2
+        let rootNode: ListNode = ListNode.init(1)
         
-//        n2.left = n3
-//        n2.right = n4
-//
-//        n4.right = n5
-//
-//        n5.right = n6
+//        var lastNode: ListNode = rootNode
+//        for i in 2...5{
+//            let n: ListNode = ListNode.init(i)
+//            lastNode.next = n
+//            lastNode = n
+//        }
         
-        let v = LeetCode.shared._124(n1)
+        var nn: ListNode? = rootNode
+        while nn != nil{
+            print(nn?.val ?? -1)
+            nn = nn?.next
+        }
         
-        print(v)
+        let v = LeetCode.shared._19(rootNode, 1)
+        
+        print(" ")
+        var n: ListNode? = v
+        while n != nil{
+            print(n?.val ?? -1)
+            n = n?.next
+        }
+        
     }
 }
